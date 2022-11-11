1847cfbef156a5590925e89df13449d2822b966e
s84559 <lilidrabi@outlook.de>
2022-11-11 14:06:47 +0100

    User change Data and Password

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,9 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import CustomUserSerializer
+from .serializers import CustomUserSerializer, ChangePasswordSerializer, UpdateUserSerializer
 
 
 class CustomUserCreate(APIView):
@@ -30,3 +31,29 @@ class LogoutAndBlacklistRefreshTokenForUserView(APIView):
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChangePasswordView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def put(self, request, format='json'):
+        serializer = ChangePasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            password = serializer.save()
+            if password:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UpdateProfileView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def put(self, request, format='json'):
+        serializer = UpdateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.save()
+            if data:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
