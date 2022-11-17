f442f4777b8382509bf53a4fe368fb19100bd4aa
Emilia D <s50507@bht-berlin.de>
2022-11-17 14:51:40 +0100

    trying to solve 401 error

--- get_outside/tests.py
+++ get_outside/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+c = Client()
+response = c.post('user/create', {'email': 'emilia.d@web.de', 'username': 'emilia', 'password': '12345678'})
+response.status_code
+""" response = c.get('/customer/details/')
+response.content """
