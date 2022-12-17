0886571f51d47e2733e53b54e63ba17e42b5f500
Josefine Hoppe <jojofine@web.de>
2022-12-17 03:12:35 +0100

    still fixing comments view

--- get_outside/models/commentsModel.py
+++ get_outside/models/commentsModel.py
@@ -1,14 +1,10 @@
 from django.db import models
 from authentication.models import CustomUser
-# from get_outside.models import mappointModel
+from get_outside.models.mappointModel import Mappoint
 
 class Comment(models.Model):
 
-    # CustomUser = userModel.CustomUser
-    # mappoint = mappointModel
-
-    # comment_id = models.CharField(max_length=255, blank=True)
-    # mappoint_id = models.ForeignKey(mappoint, on_delete= models.CASCADE)
+    mappoint_id = models.ForeignKey(Mappoint, related_name='comments', default=0, on_delete= models.CASCADE)
     author_id = models.ForeignKey(CustomUser, null=True, on_delete=models.CASCADE) # oder SET() ???
     created_at = models.DateTimeField(auto_now=True)
     text = models.CharField(max_length=255, blank=True)
