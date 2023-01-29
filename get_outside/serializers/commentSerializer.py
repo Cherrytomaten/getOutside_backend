ed8f75f24f8e430358275c3806743454e6566469
emiliadoe <emilia.d@web.de>
2023-01-29 14:14:57 +0100

    ratings url & comments fixed

--- get_outside/serializers/commentSerializer.py
+++ get_outside/serializers/commentSerializer.py
@@ -1,12 +1,10 @@
 from get_outside.models.commentsModel import Comment
 from rest_framework import serializers
-# from ..serializers.serializers import MappointSerializer
 
 # Serializers define the API representation.
 class CommentsSerializer(serializers.ModelSerializer):
     text = serializers.CharField(max_length=255)
-    # mappoint= MappointSerializer(required=False)
 
     class Meta:
         model = Comment
-        fields = ['author', 'text', 'created_at']
+        fields = '__all__' #['author', 'text', 'created_at', 'mappointPin']
--- get_outside/views/mappointView.py
+++ get_outside/views/mappointView.py
@@ -15,7 +15,7 @@ from get_outside.serializers.serializers import ImageSerializer, MappointSeriali
 
 # ViewSets define the view behavior.
 class MappointViewSet(APIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (AllowAny,)
 
     def detail_view(self, pk):
         try:
